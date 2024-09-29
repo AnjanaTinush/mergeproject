626d667ed828d040a422512e1b0d69801ea03fac
Sathsara Sithum <99238978+IT21239298@users.noreply.github.com>
2024-09-29 16:39:26 +0530

    quectionlist

--- Finalproject/Backend/app.js
+++ Finalproject/Backend/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const courseRouter = require("./Routes/courseRoute/CourseRoutes");
 const universityRouter = require("./Routes/courseRoute/UniversityRoutes");
+const questionRoutes = require("./Routes/examRoute/question.routes");
 
 const app = express();
 const cors = require("cors");
@@ -11,10 +12,12 @@ app.use(express.json());
 app.use(cors());
 app.use("/courses", courseRouter);
 app.use("/universities", universityRouter);
+app.use("/api/questions", questionRoutes);
 
-mongoose.connect("mongodb+srv://UAS:YfuHjRSAVRwncMi4@cluster0.lrlkb.mongodb.net/")
-.then(() => console.log("Connected to MongoDB"))
-.then(() => {
+mongoose
+  .connect("mongodb+srv://UAS:YfuHjRSAVRwncMi4@cluster0.lrlkb.mongodb.net/")
+  .then(() => console.log("Connected to MongoDB"))
+  .then(() => {
     app.listen(5000);
-})
-.catch((err) => console.log(err));
+  })
+  .catch((err) => console.log(err));
--- Finalproject/frontend/src/App.js
+++ Finalproject/frontend/src/App.js
@@ -2,15 +2,24 @@ import React from "react";
 import "./App.css";
 import Maincview from "./Components/courseComponent/Studentcourseview/Maincview";
 import Header from "./Components/courseComponent/Nav/Header";
-import CourseView from "./Components/courseComponent/Studentcourseview/CourseView"
+import CourseView from "./Components/courseComponent/Studentcourseview/CourseView";
 import Cportfolio from "./Components/courseComponent/Universitycouses/Cportfolio";
 import UcourseView from "./Components/courseComponent/Universitycouses/Ucousreview";
 import UpdateCourseForm from "./Components/courseComponent/Universitycouses/UpdateCourseForm";
 import AddCourseForm from "./Components/courseComponent/Universitycouses/AddCourseForm";
 
-import { Route, Routes } from 'react-router-dom';
+// import QuestionList from "./components/QuestionList";
+// import QuestionForm from "./components/QuestionForm";
+// import QuestionDetail from "./components/QuestionDetail";
+// import GeneratedPaper from "./components/GeneratedPaper";
+// import StudentResults from "./components/StudentResults";
+import QuestionList from "./Components/examComponent/QuestionList";
+import QuestionForm from "./Components/examComponent/QuestionForm";
+import QuestionDetail from "./Components/examComponent/QuestionDetail";
+import GeneratedPaper from "./Components/examComponent/GeneratedPaper";
+import StudentResults from "./Components/examComponent/StudentResults";
 
-
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
@@ -25,7 +34,13 @@ function App() {
         <Route path="course-update/:id" element={<UpdateCourseForm />} />
         <Route path="add-course/" element={<AddCourseForm />} />
 
-
+        {/* examination route */}
+        <Route path="/questionlist" element={<QuestionList />} />
+        <Route path="/create" element={<QuestionForm />} />
+        <Route path="/update/:id" element={<QuestionForm />} />
+        <Route path="/question/:id" element={<QuestionDetail />} />
+        <Route path="/generated-paper" element={<GeneratedPaper />} />
+        <Route path="/student-results" element={<StudentResults />} />
       </Routes>
     </>
   );
