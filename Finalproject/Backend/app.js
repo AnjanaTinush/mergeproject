4388e1174726fbede0c0dc4b272b8615b734d86f
AnjanaTinush <146495563+AnjanaTinush@users.noreply.github.com>
2024-09-29 17:09:08 +0530

    complete merge workers component

--- Finalproject/Backend/app.js
+++ Finalproject/Backend/app.js
@@ -1,18 +1,28 @@
 const express = require("express");
 const mongoose = require("mongoose");
+//cours
 const courseRouter = require("./Routes/courseRoute/CourseRoutes");
 const universityRouter = require("./Routes/courseRoute/UniversityRoutes");
+//workers
+const UserRoutes=require('./Routes/workRoute/UserRoute');
 
 const app = express();
 const cors = require("cors");
 
-//middleware
+//cours
 app.use(express.json());
 app.use(cors());
 app.use("/courses", courseRouter);
 app.use("/universities", universityRouter);
+app.use('/UserUploads',express.static("UserUploads/"));
 
-mongoose.connect("mongodb+srv://UAS:YfuHjRSAVRwncMi4@cluster0.lrlkb.mongodb.net/")
+//workers
+app.use('/auth',UserRoutes);
+
+
+
+
+mongoose.connect("mongodb+srv://ebuysl:ebuysl@cluster0.1awm1om.mongodb.net/curd-database")
 .then(() => console.log("Connected to MongoDB"))
 .then(() => {
     app.listen(5000);
--- Finalproject/frontend/src/App.js
+++ Finalproject/frontend/src/App.js
@@ -2,31 +2,37 @@ import React from "react";
 import "./App.css";
 import Maincview from "./Components/courseComponent/Studentcourseview/Maincview";
 import Header from "./Components/courseComponent/Nav/Header";
-import CourseView from "./Components/courseComponent/Studentcourseview/CourseView"
+import CourseView from "./Components/courseComponent/Studentcourseview/CourseView";
 import Cportfolio from "./Components/courseComponent/Universitycouses/Cportfolio";
 import UcourseView from "./Components/courseComponent/Universitycouses/Ucousreview";
 import UpdateCourseForm from "./Components/courseComponent/Universitycouses/UpdateCourseForm";
 import AddCourseForm from "./Components/courseComponent/Universitycouses/AddCourseForm";
+import Home from "./Components/workComponent/Home/Home";
+import Register from "./Components/workComponent/Register/Register";
+import Update from "./Components/workComponent/Update/Update";
 
-import { Route, Routes } from 'react-router-dom';
-
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <>
       <Header />
-      <Routes>
-        <Route path="/course-page" element={<Maincview />} />
-        <Route path="/course-view/:id" element={<CourseView />} />
+     
+        <Routes>
+          {/* Course Routes */}
+          <Route path="/course-page" element={<Maincview />} />
+          <Route path="/course-view/:id" element={<CourseView />} />
+          <Route path="university-portfolio" element={<Cportfolio />} />
+          <Route path="university-course/:id" element={<UcourseView />} />
+          <Route path="course-update/:id" element={<UpdateCourseForm />} />
+          <Route path="add-course" element={<AddCourseForm />} />
 
-        <Route path="university-portfolio" element={<Cportfolio />} />
-        <Route path="university-Course/:id" element={<UcourseView />} />
-        <Route path="course-update/:id" element={<UpdateCourseForm />} />
-        <Route path="add-course/" element={<AddCourseForm />} />
-
-
-      </Routes>
+          {/* Work Routes */}
+          <Route path="/workregister" element={<Register />} />
+          <Route path="/addworkers" element={<Home />} />
+          <Route path="/workersupdate" element={<Update />} />
+        </Routes>
+     
     </>
   );
 }
